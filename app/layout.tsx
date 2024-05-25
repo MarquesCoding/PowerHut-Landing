b9bd775ae5ad8ae9f96e323e5fda3b670849e7e8
Marques Scripps <s195139@uos.ac.uk>
2024-05-25 13:33:47 +0100

    updated URLs for Discord

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "PowerHut | Game Hosting Company",
+  title: "PowerHut | Game Hosting",
   description: "A Game Hosting Company that cares about raw performance",
 };
 
@@ -17,11 +17,21 @@ export default function RootLayout({
 }>) {
   return (
       <html lang="en">
-        <meta content="PowerHut" property="og:title"/>
-        <meta content="Raw Power Hosting" property="og:description"/>
-        <meta content="https://powerhut.pro" property="og:url"/>
-        <meta content="https://www.powerhut.pro/logo-white.png" property="og:image"/>
-        <meta content="#FF4D14" data-react-helmet="true" name="theme-color"/>
+      <meta content="https://powerhut.pro" property="og:url"/>
+      <meta content="website" property="og:type"/>
+      <meta content="PowerHut.pro" property="og:site_name"/>
+      <meta content="PowerHut | Game Hosting" property="og:title"/>
+      <meta content="en" property="og:locale"/>
+      <meta content="https://www.powerhut.pro/logo-white.png" property="og:image"/>
+      <meta
+          content="Want to play games friends, but don't have a huge budget? We offer the Latest Intel & AMD CPUs at competitive prices."
+          property="og:description"/>
+      <meta content="summary_large_image" property="twitter:card"/>
+      <meta content="https://powerhut.pro" property="twitter:url"/>
+      <meta content="PowerHut | Game Hosting" property="twitter:title"/>
+      <meta content="Want to play games friends, but don't have a huge budget? We offer the Latest Intel & AMD CPUs at competitive prices." property="twitter:description"/>
+      <meta content="https://www.powerhut.pro/logo-white.png" property="twitter:image"/>
+      <meta content="#FF4D14" data-react-helmet="true" name="theme-color"/>
       <body className={inter.className}>{children}</body>
       </html>
   );
