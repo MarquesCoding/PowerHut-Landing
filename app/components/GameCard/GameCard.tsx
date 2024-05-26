820767dee04dafe3407a3a10468256c32c32d5c6
Marques Scripps <s195139@uos.ac.uk>
2024-05-26 01:55:02 +0100

    fixed footer

--- app/components/GameCard/GameCard.tsx
+++ app/components/GameCard/GameCard.tsx
@@ -8,7 +8,7 @@ const GameCard: React.FC<GameCardProps> = ({ imageSrc }) => {
         <div className="w-auto min-w-72 relative hover:-translate-y-1 duration-300 group rounded-2xl">
             <div
                 className="w-full min-w-96 absolute bg-dot-white/[0.13] rounded-2xl z-0 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
-            <div className="w-full h-full absolute bg-gradient-to-t to-[#FF4D14]/20 from-transparent group-hover:opacity-0 duration-300">
+            <div className="w-full h-full absolute bg-gradient-to-t to-[#FF4D14]/20 rounded-2xl from-transparent group-hover:opacity-0 duration-300">
 
             </div>
             <img src={imageSrc} className={"w-72 h-96 bg-zinc-800 rounded-xl"} alt='image'/>
