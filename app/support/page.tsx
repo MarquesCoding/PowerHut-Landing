dffe97b37c8875eb1d45c2b0888f1a76725f2ca6
Marques Scripps <s195139@uos.ac.uk>
2024-06-28 17:52:23 +0100

    partner

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -34,124 +34,169 @@ export default function Page() {
     Intercom({
         app_id: 'dcisso7v',
     });
+
+    const container = {
+        hidden: { opacity: 1, translateY: -40 },
+        visible: {
+            opacity: 1,
+            translateY: 0,
+            transition: {
+                staggerChildren: 0.5,
+            },
+        },
+    };
+
+    const item = {
+        hidden: { opacity: 0, y: 50 },
+        visible: { opacity: 1, y: 0 },
+    };
     return (
         <div className="w-full h-full flex flex-col">
             <Promo/>
-            <motion.div
-                initial={{
-                    opacity: 0
-                }}
-                animate={{
-                    opacity: 100
-                }}
-                transition={{
-                    ease: "linear",
-                    opacity: {duration: 1}
-                }}
-
-                className="w-full h-[50vh] bg-gradient-to-tr from-[#FF4D14]/10 to-transparent flex items-center flex-col bg-opacity-50 relative">
-                <div className="max w-full">
-                    <NavigationBar/>
-                </div>
-                <div
-                    className="w-full h-full flex items-center justify-center relative z-10 flex-col pt-48 text-center">
-                    <motion.div
-                        initial={{
-                            opacity: 0,
-                        }}
-                        animate={{
-                            opacity: 100,
-                        }}
-                        transition={{
-                            ease: "linear",
-                            opacity: {duration: 1}
-                        }}
-                        className="flex flex-col items-center"
-                    >
-                        <p className="text-3xl lg:text-8xl font-bold">How can <span
-                            className="text-[#FF4D14]">We </span> help You?
-                        </p>
-                        <div className="max-w-2xl mt-4 flex flex-col gap-4 opacity-50">
-                            <p>Our Support Team is ready for all your questions, at almost any hour during the day.
-                                Outstanding Support is what we believe in, and what you deserve!</p>
-                        </div>
-                        <b className="italic mt-4">NOTE - We are currently not available 24/7. <br/>We typically reply between 9AM GMT - 11PM GMT</b>
-                    </motion.div>
-                </div>
-            </motion.div>
-            <motion.div
-                initial={{
-                    opacity: 0
-                }}
-                animate={{
-                    opacity: 100
-                }}
-                transition={{
-                    ease: "linear",
-                    opacity: {duration: 1}
-                }}
-
-                className="w-full h-auto flex items-center justify-center bg-gradient-to-tl to-[#FF4D14]/10 from-transparent bg-opacity-50 pb-10">
-                <div
-                    className="w-full lg:w-auto m-4 lg:m-0 h-auto select-none rounded-2xl bg-[#141414]/90 border border-white/10 relative flex flex-col lg:flex-row items-center justify-center duration-300">
+            <div className="w-full h-auto background-tr flex items-center justify-center">
+                <div className="w-full h-full max pb-4 xl:pb-0">
                     <div
-                        className="w-full h-full absolute bg-dot-white/[0.23] rounded-2xl z-0 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
-                    <div className="flex flex-col justify-between p-8 gap-4 items-center relative z-10">
-                        <div className="text-center flex flex-col gap-4 items-center">
-                            <p className="text-md font-semibold md:text-2xl">Discord</p>
-                            <div
-                                className="w-20 min-w-20 h-20 rounded-lg flex items-center justify-center text-[#FF4D14]">
-                                <FaDiscord size={64}/>
+                        className="w-full h-full flex items-center justify-center relative z-10 flex-col">
+                        <NavigationBar/>
+                        <div className="flex flex-col w-full px-8 xl:p-8">
+                            <motion.div
+                                className="text-5xl md:text-6xl xl:text-8xl 2xl:text-9xl font-bold selection:bg-white"
+                                variants={container}
+                                initial="hidden"
+                                animate="visible"
+                            >
+                                {/*<motion.span*/}
+                                {/*    className="text-zinc-400 selection:bg-white"*/}
+                                {/*    variants={item}*/}
+                                {/*    transition={{delay: 0.1}}*/}
+                                {/*>*/}
+                                {/*    PowerHut*/}
+                                {/*    {' '}*/}
+                                {/*</motion.span>*/}
+
+                                <motion.span
+                                    className="text-zinc-400 selection:bg-white"
+                                    variants={item}
+                                    transition={{delay: 0.2}}
+                                >
+                                    How
+                                    {' '}
+                                </motion.span>
+                                <motion.span
+                                    className="text-zinc-400 selection:bg-white"
+                                    variants={item}
+                                    transition={{delay: 0.3}}
+                                >
+                                    can
+                                    {' '}
+                                </motion.span>
+                                <motion.span
+                                    className="text-main selection:bg-white"
+                                    variants={item}
+                                    transition={{delay: 0.4}}
+                                >
+                                    We
+                                    {' '}
+                                </motion.span>
+                                <motion.span
+                                    className="text-zinc-400 selection:bg-white"
+                                    variants={item}
+                                    transition={{delay: 0.5}}
+                                >
+                                    help
+                                    {' '}
+                                </motion.span>
+                                <motion.span
+                                    className="text-zinc-400 selection:bg-white"
+                                    variants={item}
+                                    transition={{delay: 0.6}}
+                                >
+                                    You?
+                                </motion.span>
+                            </motion.div>
+                            <div>
+                                <p className="mt-8 max-w-[40rem] selection:bg-main">
+                                    Our Support Team is ready for all your questions, at almost any hour during the day.
+                                    Outstanding Support is what we believe in, and what you deserve!
+                                </p>
+                                <p className="mt-8 max-w-[40rem] opacity-50 selection:bg-main">
+                                    NOTE - We are currently not available 24/7.
+                                    We typically reply between 9AM GMT - 11PM GMT ~ <b>(Current Time
+                                    : {new Date().getUTCHours()}:{new Date().getUTCMinutes()})</b>
+                                </p>
                             </div>
-                            <p className="text-sm md:text-md opacity-50 max-w-xs">Response Time</p>
-                            <p className="text-sm md:text-lg max-w-xs">Generally 2-3 minutes</p>
-                            <p className="text-sm md:text-lg text-[#FF4D14] max-w-xs mb-4">{isTimeWithinRange() ? 'Online Now' : 'Offline'}</p>
-                            <a href="https://discord.gg/invite/powerhut">
-                                <Button variant="secondary">
-                                    Join Server
-                                </Button>
-                            </a>
                         </div>
                     </div>
-                    <div className="w-40 h-0.5 lg:w-0.5 lg:h-40 bg-white rounded-lg opacity-5 relative flex"/>
-                    <div className="flex flex-col justify-between p-8 gap-4 items-center relative z-10">
-                        <div className="text-center flex flex-col gap-4 items-center">
-                            <p className="text-md font-semibold md:text-2xl">Billing Area</p>
-                            <div
-                                className="w-20 min-w-20 h-20 rounded-lg flex items-center justify-center text-[#FF4D14]">
-                                <LuTicket size={64}/>
-                            </div>
-                            <p className="text-sm md:text-md opacity-50 max-w-xs">Response Time</p>
-                            <p className="text-sm md:text-lg max-w-xs">Generally under 2 hours</p>
-                            <p className="text-sm md:text-lg text-[#FF4D14] max-w-xs mb-4">{isTimeWithinRange() ? 'Online Now' : 'Offline'}</p>
-                            <a href="https://billing.powerhut.pro/contact.php">
-                                <Button variant="secondary">
-                                    Create a Ticket
-                                </Button>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="w-40 h-0.5 lg:w-0.5 lg:h-40 bg-white rounded-lg opacity-5 relative flex"/>
-                    <div className="flex flex-col justify-between p-8 gap-4 items-center relative z-10">
-                        <div className="text-center flex flex-col gap-4 items-center">
-                            <p className="text-md font-semibold md:text-2xl">Knowledgebase</p>
-                            <div
-                                className="w-20 min-w-20 h-20 rounded-lg flex items-center justify-center text-[#FF4D14]">
-                                <FiBook size={64}/>
-                            </div>
-                            <p className="text-sm md:text-md opacity-50 max-w-xs">Response Time</p>
-                            <p className="text-sm md:text-lg max-w-xs">Always Available</p>
-                            <p className="text-sm md:text-lg text-[#FF4D14] max-w-xs mb-4">Online Now!</p>
-                            <a href="https://billing.powerhut.pro/index.php?rp=/knowledgebase">
-                                <Button variant="secondary">
-                                    View Articles
-                                </Button>
-                            </a>
-                        </div>
-                    </div>
+                    {/*<BackgroundBeams/>*/}
                 </div>
-            </motion.div>
-            <FAQ/>
+            </div>
+            <div className="background-br">
+                <motion.div className="w-full h-auto flex p-8 pb-10 max">
+                    <div
+                        className="w-full lg:w-full m-4 lg:m-0 h-auto select-none rounded-2xl bg-[#141414]/90 border border-white/10 relative flex flex-col lg:flex-row items-center justify-between px-0 lg:px-40 duration-300">
+                        <div
+                            className="w-full h-full absolute bg-dot-white/[0.23] rounded-2xl z-0 -mx-40 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
+                        <div className="flex flex-col justify-between p-8 gap-4 items-center relative z-10">
+                            <div className="text-center flex flex-col gap-4 items-center">
+                                <p className="text-md font-semibold md:text-2xl">Discord</p>
+                                <div
+                                    className="w-20 min-w-20 h-20 rounded-lg flex items-center justify-center text-[#FF4D14]">
+                                    <FaDiscord size={64}/>
+                                </div>
+                                <p className="text-sm md:text-md opacity-50 max-w-xs">Response Time</p>
+                                <p className="text-sm md:text-lg max-w-xs">Generally 2-3 minutes</p>
+                                <p className="text-sm md:text-lg text-[#FF4D14] max-w-xs mb-4">{isTimeWithinRange() ? 'Online Now' : 'Offline'}</p>
+                                <a href="https://discord.gg/invite/powerhut">
+                                    <Button variant="secondary">
+                                        Join Server
+                                    </Button>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="w-40 h-0.5 lg:w-0.5 lg:h-40 bg-white rounded-lg opacity-5 relative flex"/>
+                        <div className="flex flex-col justify-between p-8 gap-4 items-center relative z-10">
+                            <div className="text-center flex flex-col gap-4 items-center">
+                                <p className="text-md font-semibold md:text-2xl">Billing Area</p>
+                                <div
+                                    className="w-20 min-w-20 h-20 rounded-lg flex items-center justify-center text-[#FF4D14]">
+                                    <LuTicket size={64}/>
+                                </div>
+                                <p className="text-sm md:text-md opacity-50 max-w-xs">Response Time</p>
+                                <p className="text-sm md:text-lg max-w-xs">Generally under 2 hours</p>
+                                <p className="text-sm md:text-lg text-[#FF4D14] max-w-xs mb-4">{isTimeWithinRange() ? 'Online Now' : 'Offline'}</p>
+                                <a href="https://billing.powerhut.pro/contact.php">
+                                    <Button variant="secondary">
+                                        Create a Ticket
+                                    </Button>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="w-40 h-0.5 lg:w-0.5 lg:h-40 bg-white rounded-lg opacity-5 relative flex"/>
+                        <div className="flex flex-col justify-between p-8 gap-4 items-center relative z-10">
+                            <div className="text-center flex flex-col gap-4 items-center">
+                                <p className="text-md font-semibold md:text-2xl">Knowledgebase</p>
+                                <div
+                                    className="w-20 min-w-20 h-20 rounded-lg flex items-center justify-center text-[#FF4D14]">
+                                    <FiBook size={64}/>
+                                </div>
+                                <p className="text-sm md:text-md opacity-50 max-w-xs">Response Time</p>
+                                <p className="text-sm md:text-lg max-w-xs">Always Available</p>
+                                <p className="text-sm md:text-lg text-[#FF4D14] max-w-xs mb-4">Online Now!</p>
+                                <a href="https://billing.powerhut.pro/index.php?rp=/knowledgebase">
+                                    <Button variant="secondary">
+                                        View Articles
+                                    </Button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </motion.div>
+            </div>
+            <div className="background-tr">
+                <div className="flex max flex-row  md:h-auto w-full px-8 xl:p-8">
+                    <FAQ/>
+                </div>
+            </div>
             <Footer/>
         </div>
     )
